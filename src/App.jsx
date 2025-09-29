5dad5c87e98f37920925467dc605735e74ce6fe8
Rhitesh Kotekar <rhiteshkotekar@gmail.com>
2025-09-29 22:57:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { React } from "react";
+
+import Chatbot from "./components/smth";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Chatbot />
+    </>
+  );
+}
+
+export default App;
